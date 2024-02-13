f302e8fbc33338a4f75a03c85f466a4593733074
Starflight-code <sparkr123@gmail.com>
2024-02-12 22:18:56 -0600

    Added reddit handler, added generalized post struct Added the reddit handler without any error handling for null JSON checks. Added a generalized post struct for return from handler.

--- Scheduling/HandlerArgs.cs
+++ Scheduling/HandlerArgs.cs
@@ -1,19 +1,26 @@
 using System.Reflection;
 using Discord.WebSocket;
+using StarBot.Caching;
 
 namespace StarBot.Scheduling;
 
 struct HandlerArgs {
     public string iteratorKey;
     public string channelKey;
+    public string cacheKey;
     public DiscordSocketClient client;
     public Database data;
     public string resourceLocation;
-    public HandlerArgs(string iteratorKey, string channelKey, DiscordSocketClient client, Database data, string resourceLocation) {
+    public ulong guildID;
+    public MemoryCacheManager cache;
+    public HandlerArgs(string iteratorKey, string channelKey, string cacheKey, DiscordSocketClient client, Database data, string resourceLocation, ulong guildID, MemoryCacheManager cache) {
         this.iteratorKey = iteratorKey;
         this.channelKey = channelKey;
+        this.cacheKey = cacheKey;
         this.client = client;
         this.data = data;
         this.resourceLocation = resourceLocation;
+        this.guildID = guildID;
+        this.cache = cache;
     }
 }
