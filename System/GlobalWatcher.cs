41784beaa09e50e15b735c7587f31fcfce0af14f
Starflight-code <sparkr123@gmail.com>
2024-03-22 20:09:13 -0500

    Impliments global watcher registration system Adds the finishing touches to the global watcher system, allowing registration checking. Existing commands should not be updated.

--- System/GlobalWatcher.cs
+++ System/GlobalWatcher.cs
@@ -6,7 +6,7 @@ using Newtonsoft.Json;
 using StarBot;
 
 public class Watcher {
-    public List<string> commandIDsMissing = new();
+    public List<string> commandNamesMissing = new();
     public struct Command {
         public enum CommandType {
             message,
@@ -38,13 +38,13 @@ public class Watcher {
         string[]? currentGuilds = data.getKeys("watcher-id");
         HashSet<string> guilds = currentGuilds.ToHashSet();
         List<SocketApplicationCommand> commandsOnDiscord = new();
-        commandIDsMissing.AddRange(data.getKeys("watcher-name"));
+        commandNamesMissing.AddRange(data.getKeys("watcher-name"));
         foreach (string guildID in guilds) {
             commandsOnDiscord.AddRange(await client.GetGuild(ulong.Parse(guildID)).GetApplicationCommandsAsync());
         }
 
         for (int i = 0; i < commandsOnDiscord.Count; i++) {
-            commandIDsMissing.Remove(commandsOnDiscord[i].ApplicationId.ToString());
+            commandNamesMissing.Remove(commandsOnDiscord[i].Name.ToString());
         }
     }
     List<Command> registeredCommands = new();
@@ -55,7 +55,15 @@ public class Watcher {
         data.setValue(commandID.ToString(), slashData.Name.Value + "-S", "watcher-name");
         //data.setValue(commandID.ToString(), JsonConvert.SerializeObject(slashData), guildID); TODO: Add in global watcher database sync, to remove the need to re-register commands every launch
     }
-    public void RegisterCommand(ulong guildID, Database data, SlashCommandProperties? slashData) {
+    public async void RegisterCommand(DiscordSocketClient client, ulong guildID, Database data, SlashCommandProperties? slashData) {
+        if (commandNamesMissing.Contains(slashData.Name.Value)) { // TODO: Fix invalid logic here, should check for matching names, instead of an array of IDs
+            commandNamesMissing.Remove(slashData.Name.Value);
+            SocketApplicationCommand command = await client.GetGuild(guildID).CreateApplicationCommandAsync(slashData); // TODO: add code to fully register command by clearing off stale data and registering command normally
+            registeredCommands.Add(new(command.Id, guildID, slashData));
+            string currentData = data.fetchValue(guildID.ToString(), "watcher");
+            data.setValue(guildID.ToString(), currentData == "" ? currentData : currentData + "-" + command.Id.ToString(), "watcher-id");
+            data.setValue(command.Id.ToString(), slashData.Name.Value + "-M", "watcher-name");
+        }
         //registeredCommands.Add(new(commandID, guildID, slashData));
         //string currentData = data.fetchValue(guildID.ToString(), "watcher");
         //data.setValue(guildID.ToString(), currentData == "" ? currentData : currentData + "-" + commandID.ToString(), "watcher-id");
@@ -69,10 +77,14 @@ public class Watcher {
         data.setValue(commandID.ToString(), messageData.Name.Value + "-M", "watcher-name");
         //data.setValue(commandID.ToString(), JsonConvert.SerializeObject(messageData), guildID);
     }
-    public void RegisterCommand(DiscordSocketClient client, ulong guildID, Database data, MessageCommandProperties? messageData) {
-        if (commandIDsMissing.Contains(messageData.Name.Value)) { // TODO: Fix invalid logic here, should check for matching names, instead of an array of IDs
-            commandIDsMissing.Remove(messageData.Name.Value);
-            client.GetGuild(guildID).CreateApplicationCommandAsync(messageData); // TODO: add code to fully register command by clearing off stale data and registering command normally
+    public async void RegisterCommand(DiscordSocketClient client, ulong guildID, Database data, MessageCommandProperties? messageData) {
+        if (commandNamesMissing.Contains(messageData.Name.Value)) { // TODO: Fix invalid logic here, should check for matching names, instead of an array of IDs
+            commandNamesMissing.Remove(messageData.Name.Value);
+            SocketApplicationCommand command = await client.GetGuild(guildID).CreateApplicationCommandAsync(messageData); // TODO: add code to fully register command by clearing off stale data and registering command normally
+            registeredCommands.Add(new(command.Id, guildID, messageData));
+            string currentData = data.fetchValue(guildID.ToString(), "watcher");
+            data.setValue(guildID.ToString(), currentData == "" ? currentData : currentData + "-" + command.Id.ToString(), "watcher-id");
+            data.setValue(command.Id.ToString(), messageData.Name.Value + "-M", "watcher-name");
         }
         //registeredCommands.Add(new(commandID, guildID, messageData));
         //string currentData = data.fetchValue(guildID.ToString(), "watcher");
@@ -101,31 +113,32 @@ public class Watcher {
             List<int> registerAgainIndexes = new();
             bool foundID = false;
             for (int i = 0; i < registeredCommands.Count(); i++) { // finds commands Discord removed or were otherwise removed some other way
-            if (guild.Id == registeredCommands[i].guildID) {
-                for (int j = 0; j < commandList.Count; j++) {
-                    if (commandList[j].Id == registeredCommands[i].id) {
-                        foundID = true;
-                        break;
+                if (guild.Id == registeredCommands[i].guildID) {
+                    for (int j = 0; j < commandList.Count; j++) {
+                        if (commandList[j].Id == registeredCommands[i].id) {
+                            foundID = true;
+                            break;
+                        }
                     }
+                    if (!foundID) {
+                        registerAgain.Add(registeredCommands[i]);
+                        registerAgainIndexes.Add(i);
+                    }
+                    foundID = false;
                 }
-                if (!foundID) {
-                    registerAgain.Add(registeredCommands[i]);
-                    registerAgainIndexes.Add(i);
-                }
-                foundID = false;
-            }}
+            }
             SocketApplicationCommand command;
             for (int i = 0; i < registerAgain.Count; i++) { // adds re-registered commands back
-                switch(registerAgain[i].type) {
+                switch (registerAgain[i].type) {
                     case Command.CommandType.slash:
-                    command = await guild.CreateApplicationCommandAsync(registerAgain[i].slashData);
-                    registeredCommands.Add(new(command.Id, guild.Id, registerAgain[i].slashData));
+                        command = await guild.CreateApplicationCommandAsync(registerAgain[i].slashData);
+                        registeredCommands.Add(new(command.Id, guild.Id, registerAgain[i].slashData));
 
-                    break;
+                        break;
                     case Command.CommandType.message:
-                    command = await guild.CreateApplicationCommandAsync(registerAgain[i].messageData);
-                    registeredCommands.Add(new(command.Id, guild.Id, registerAgain[i].messageData));
-                    break;
+                        command = await guild.CreateApplicationCommandAsync(registerAgain[i].messageData);
+                        registeredCommands.Add(new(command.Id, guild.Id, registerAgain[i].messageData));
+                        break;
                 }
             }
             for (int i = registerAgainIndexes.Count - 1; 0 <= i; i--) {
