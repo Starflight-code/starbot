aa4d2004394b56540c595d3e6c71a6829d0809ea
Starflight-code <sparkr123@gmail.com>
2024-04-09 22:44:04 -0500

    Generalized db code, prep for blacklist Generalized db code, making it easier to abstract to more efficient data structures. Also allows more code re-use, keeping the codebase (pretty) small.

--- DataStorage/Database.cs
+++ DataStorage/Database.cs
@@ -2,8 +2,8 @@
 
 namespace StarBot {
     public class Database {
-        Dictionary<ulong, DatabaseObject> guildDatabases; // guildID will find associated DatabaseObject
-        Dictionary<string, DatabaseObject> otherDatabases;
+        Dictionary<ulong, DatabaseObject<string>> guildDatabases; // guildID will find associated DatabaseObject
+        Dictionary<string, DatabaseObject<string>> otherDatabases;
         //DatabaseObject data;
         public List<ulong> guilds;
         public Database(DiscordSocketClient client) {
@@ -15,15 +15,15 @@ namespace StarBot {
             foreach (SocketGuild guild in client.Guilds) {
                 ulong guildID = guild.Id;
                 guilds.Add(guildID);
-                guildDatabases.Add(guildID, new DatabaseObject(guildID));
+                guildDatabases.Add(guildID, new DatabaseObject<string>(guildID));
             }
             foreach (string path in Directory.GetFiles(Config.DATABASE_DIRECTORY)) {
                 string name = Path.GetFileNameWithoutExtension(path);
                 try {
-                ulong guildID = ulong.Parse(name);
-                if (!guilds.Contains(guildID)) {
-                    otherDatabases.Add(name, new(name));
-                }
+                    ulong guildID = ulong.Parse(name);
+                    if (!guilds.Contains(guildID)) {
+                        otherDatabases.Add(name, new(name));
+                    }
                 } catch {
                     otherDatabases.Add(name, new(name));
                 }
@@ -39,71 +39,69 @@ namespace StarBot {
                 guildDatabases.Add(dbKey, new(dbKey));
             }
         }
-        public string fetchValue(string key, ulong guildID) {
+        public string? fetchValue(string key, ulong guildID) {
             key = Validation.preProcessValue(key);
-            guildDatabases.TryGetValue(guildID, out DatabaseObject? targetDatabase);
+            guildDatabases.TryGetValue(guildID, out DatabaseObject<string>? targetDatabase);
             if (targetDatabase == null) { return ""; }
             return targetDatabase.get(key);
         }
-        public string fetchValue(string key, string identifier) {
+        public string? fetchValue(string key, string identifier) {
             key = Validation.preProcessValue(key);
-            otherDatabases.TryGetValue(identifier, out DatabaseObject? targetDatabase);
+            otherDatabases.TryGetValue(identifier, out DatabaseObject<string>? targetDatabase);
             if (targetDatabase == null) { return ""; }
             return targetDatabase.get(key);
         }
 
         public string[]? getKeys(ulong guildID) {
-            guildDatabases.TryGetValue(guildID, out DatabaseObject? targetDatabase);
+            guildDatabases.TryGetValue(guildID, out DatabaseObject<string>? targetDatabase);
             if (targetDatabase == null) { return default; }
             return targetDatabase.getAllKeys();
         }
 
         public string[]? getKeys(string identifier) {
-            otherDatabases.TryGetValue(identifier, out DatabaseObject? targetDatabase);
+            otherDatabases.TryGetValue(identifier, out DatabaseObject<string>? targetDatabase);
             if (targetDatabase == null) { return default; }
             return targetDatabase.getAllKeys();
         }
-        public string[]? getValues(ulong guildID) {
-            guildDatabases.TryGetValue(guildID, out DatabaseObject? targetDatabase);
+        public string?[]? getValues(ulong guildID) {
+            guildDatabases.TryGetValue(guildID, out DatabaseObject<string>? targetDatabase);
             if (targetDatabase == null) { return default; }
             return targetDatabase.getAllValues();
         }
 
-        public string[]? getValues(string identifier) {
-            otherDatabases.TryGetValue(identifier, out DatabaseObject? targetDatabase);
+        public string?[]? getValues(string identifier) {
+            otherDatabases.TryGetValue(identifier, out DatabaseObject<string>? targetDatabase);
             if (targetDatabase == null) { return default; }
             return targetDatabase.getAllValues();
         }
         public void removeValue(string key, ulong guildID) {
             key = Validation.preProcessValue(key);
-            guildDatabases.TryGetValue(guildID, out DatabaseObject? targetDatabase);
+            guildDatabases.TryGetValue(guildID, out DatabaseObject<string>? targetDatabase);
             if (targetDatabase == null) { return; }
             targetDatabase.remove(key);
         }
 
         public void removeValue(string key, string identifier) {
             key = Validation.preProcessValue(key);
-            otherDatabases.TryGetValue(identifier, out DatabaseObject? targetDatabase);
+            otherDatabases.TryGetValue(identifier, out DatabaseObject<string>? targetDatabase);
             if (targetDatabase == null) { return; }
             targetDatabase.remove(key);
         }
         public async Task iterateValue(string key, ulong guildID, bool updateDB = false) {
             key = Validation.preProcessValue(key);
             await initializeIterator(key, guildID);
-            string value = fetchValue(key, guildID);
+            string? value = fetchValue(key, guildID);
             int number;
             try {
                 number = int.Parse(value);
             } catch (FormatException e) {
                 Console.WriteLine(e.Message + "\nPossible database corruption detected, fix this ASAP!");
-                return;
-                //throw e;
+                throw;
             } catch (ArgumentNullException e) {
                 Console.WriteLine(e.Message + "\nPossible database corruption detected, fix this ASAP!");
-                return;
-                //throw e;
+                throw;
             }
-            guildDatabases.TryGetValue(guildID, out DatabaseObject? data);
+            guildDatabases.TryGetValue(guildID, out DatabaseObject<string>? data);
             data.add(key, (++number).ToString());
             if (updateDB) {
                 await data.updateSelf(guildID);
@@ -113,7 +111,7 @@ namespace StarBot {
         public async Task iterateValue(string key, string identifier, bool updateDB = false) {
             key = Validation.preProcessValue(key);
             await initializeIterator(key, identifier);
-            string value = fetchValue(key, identifier);
+            string? value = fetchValue(key, identifier);
             int number;
             try {
                 number = int.Parse(value);
@@ -126,7 +124,7 @@ namespace StarBot {
                 return;
                 //throw e;
             }
-            otherDatabases.TryGetValue(identifier, out DatabaseObject? data);
+            otherDatabases.TryGetValue(identifier, out DatabaseObject<string>? data);
             data.add(key, (++number).ToString());
             if (updateDB) {
                 await data.updateSelf(identifier);
@@ -137,7 +135,7 @@ namespace StarBot {
             if (guildID == null) { return; }
             value = Validation.preProcessValue(value);
             key = Validation.preProcessValue(key);
-            guildDatabases.TryGetValue((ulong)guildID, out DatabaseObject? targetDatabase);
+            guildDatabases.TryGetValue((ulong)guildID, out DatabaseObject<string>? targetDatabase);
             if (targetDatabase == null) { return; }
             targetDatabase.add(key, value);
             if (updateDB) {
@@ -149,7 +147,7 @@ namespace StarBot {
             if (identifier == null) { return; }
             value = Validation.preProcessValue(value);
             key = Validation.preProcessValue(key);
-            otherDatabases.TryGetValue(identifier, out DatabaseObject? targetDatabase);
+            otherDatabases.TryGetValue(identifier, out DatabaseObject<string>? targetDatabase);
             if (targetDatabase == null) { return; }
             targetDatabase.add(key, value);
             if (updateDB) {
@@ -158,12 +156,12 @@ namespace StarBot {
         }
 
         public async Task updateDB(ulong guildID) {
-            guildDatabases.TryGetValue(guildID, out DatabaseObject? targetDatabase);
+            guildDatabases.TryGetValue(guildID, out DatabaseObject<string>? targetDatabase);
             if (targetDatabase == null) { return; }
             await targetDatabase.updateSelf(guildID);
         }
         public async Task updateDB(string identifier) {
-            otherDatabases.TryGetValue(identifier, out DatabaseObject? targetDatabase);
+            otherDatabases.TryGetValue(identifier, out DatabaseObject<string>? targetDatabase);
             if (targetDatabase == null) { return; }
             await targetDatabase.updateSelf(identifier);
         }
@@ -171,7 +169,7 @@ namespace StarBot {
 
         public async Task initializeIterator(string key, ulong guildID, int startingValue = 1, bool updateDB = false) {
             key = Validation.preProcessValue(key);
-            guildDatabases.TryGetValue(guildID, out DatabaseObject? targetDatabase);
+            guildDatabases.TryGetValue(guildID, out DatabaseObject<string>? targetDatabase);
             if (targetDatabase == null) { return; }
             if (targetDatabase.doesKeyExist(key)) {
                 return;
@@ -184,7 +182,7 @@ namespace StarBot {
         }
         public async Task initializeIterator(string key, string identifier, int startingValue = 1, bool updateDB = false) {
             key = Validation.preProcessValue(key);
-            otherDatabases.TryGetValue(identifier, out DatabaseObject? targetDatabase);
+            otherDatabases.TryGetValue(identifier, out DatabaseObject<string>? targetDatabase);
             if (targetDatabase == null) { return; }
             if (targetDatabase.doesKeyExist(key)) {
                 return;
@@ -196,24 +194,24 @@ namespace StarBot {
             }
         }
         public string getSerializedDB(ulong guildID) {
-            guildDatabases.TryGetValue(guildID, out DatabaseObject? targetDatabase);
+            guildDatabases.TryGetValue(guildID, out DatabaseObject<string>? targetDatabase);
             if (targetDatabase == null) { return ""; }
             return targetDatabase.getSerializedSelf();
         }
 
         public string getSerializedDB(string identifier) {
-            otherDatabases.TryGetValue(identifier, out DatabaseObject? targetDatabase);
+            otherDatabases.TryGetValue(identifier, out DatabaseObject<string>? targetDatabase);
             if (targetDatabase == null) { return ""; }
             return targetDatabase.getSerializedSelf();
         }
         public void setSerializedDB(string serializedJSON, ulong guildID) {
-            guildDatabases.TryGetValue(guildID, out DatabaseObject? targetDatabase);
+            guildDatabases.TryGetValue(guildID, out DatabaseObject<string>? targetDatabase);
             if (targetDatabase == null) { return; }
             targetDatabase.populateSelfSerialized(serializedJSON);
         }
 
         public void setSerializedDB(string serializedJSON, string identifier) {
-            otherDatabases.TryGetValue(identifier, out DatabaseObject? targetDatabase);
+            otherDatabases.TryGetValue(identifier, out DatabaseObject<string>? targetDatabase);
             if (targetDatabase == null) { return; }
             targetDatabase.populateSelfSerialized(serializedJSON);
         }
