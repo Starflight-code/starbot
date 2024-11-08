25e1cf4b71cecb71304e8a36ed9a9525b24b1394
Starflight-Code <sparkr123@gmail.com>
2024-11-08 10:22:55 -0600

    Finish: Migration to Diesel-rs Finished migration to Diesel-rs, stablized after testing and ready for production.

--- src/database.rs
+++ src/database.rs
@@ -26,7 +26,7 @@ pub struct Automation {
 }*/
 
 pub async fn establish_connection() -> SqliteConnection {
-    let database_url = "./data-new.db";
+    let database_url = "./data.db";
     SqliteConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
